232054a60fcf773226537542b933b4e6d41e8ed8
Jaw <tuyweber@gmail.com>
2023-01-10 01:23:44 -0300

    Following Actix Web documentation setted mini app to run

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,36 @@
 extern crate diesel;
 extern crate dotenvy;
 
+use actix_web::{web, App, HttpResponse, HttpServer, Responder, get, post};
 
 mod db_config;
 
-fn main() {
-    let connection = db_config::establish_connection();
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    //let db_connection = db_config::establish_connection();
+
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
